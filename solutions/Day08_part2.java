e1556c38ae887d68b393181f882eefc48218e1d7
Pawel Iskra <52103903+Pawel-Iskra@users.noreply.github.com>
2020-06-11 15:57:55 +0200

    Update Day08_part2.java

--- solutions/Day08_part2.java
+++ solutions/Day08_part2.java
@@ -4,24 +4,17 @@ import java.io.InputStreamReader;
 
 public class Day8_part2 {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         char[] in = br.readLine().toCharArray();
         int amount = in.length;
-
         int width = 25;
         int hight = 6;
         int size = width * hight;
         int layers = amount / size;
-
-
         for (int i = 0; i < hight; i++) {
-
             for (int j = 0; j < width; j++) {
-
                 for (int k = 0; k < layers; k++) {
-
                     int index = k * size + i * width + j;
                     if (in[index] != '2') {
                         if (in[index] == '0') System.out.print(" ");
@@ -32,7 +25,5 @@ public class Day8_part2 {
             }
             System.out.println();
         }
-
-
     }
 }
