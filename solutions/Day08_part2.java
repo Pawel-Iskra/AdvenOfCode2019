a17d67ebb0e2dd95c73518cced91530fef0341d9
Pawel Iskra <52103903+Pawel-Iskra@users.noreply.github.com>
2020-06-11 15:58:13 +0200

    Update Day08_part2.java

--- solutions/Day08_part2.java
+++ solutions/Day08_part2.java
@@ -12,9 +12,11 @@ public class Day8_part2 {
         int hight = 6;
         int size = width * hight;
         int layers = amount / size;
+        
         for (int i = 0; i < hight; i++) {
             for (int j = 0; j < width; j++) {
                 for (int k = 0; k < layers; k++) {
+                    
                     int index = k * size + i * width + j;
                     if (in[index] != '2') {
                         if (in[index] == '0') System.out.print(" ");
