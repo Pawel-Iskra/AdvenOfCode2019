23021af5451cd1251a872b46656290b430b8da46
Pawel Iskra <52103903+Pawel-Iskra@users.noreply.github.com>
2020-06-11 15:44:51 +0200

    Update Day06_part2.java

--- solutions/Day06_part2.java
+++ solutions/Day06_part2.java
@@ -6,71 +6,52 @@ import java.util.List;
 
 public class Day6_part2 {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         List<String> left = new ArrayList<>();
         List<String> right = new ArrayList<>();
-
         String line;
         while ((line = br.readLine()) != null) {
-
             String[] input = line.split("[)]");
             left.add(input[0]);
             right.add(input[1]);
         }
-
-
         int indYou = right.indexOf("YOU");
         int indSan = right.indexOf("SAN");
-
         ///// PATH: YOU -> COM /////
-
         List<String> pathYou = new ArrayList<>();
         String planet = left.get(indYou);
         pathYou.add(planet);
-
         int size = right.size();
         while (!planet.equals("COM")) {
-
             int tempIndex = -1;
             for (int j = 0; j < size; j++) {
                 if (right.get(j).equals(planet)) tempIndex = j;
             }
-
             planet = left.get(tempIndex);
             pathYou.add(planet);
         }
-
-
         ///// PATH: SAN -> COM /////
         List<String> pathSan = new ArrayList<>();
         planet = left.get(indSan);
         pathSan.add(planet);
-
         size = right.size();
         while (!planet.equals("COM")) {
-
             int tempIndex = -1;
             for (int j = 0; j < size; j++) {
                 if (right.get(j).equals(planet)) tempIndex = j;
             }
-
             planet = left.get(tempIndex);
             pathSan.add(planet);
         }
-
         ///// FINDING FIRST COMMON PLANET /////
         int sizePathYou = pathYou.size();
         int sizePathSan = pathSan.size();
-
         int answer = -1;
         boolean flag = false;
         for (int i = 0; i < sizePathYou; i++) {
-
             if (flag) break;
             String elem = pathYou.get(i);
-
             for (int j = 0; j < sizePathSan; j++) {
                 if (pathSan.get(j).equals(elem)) {
                     flag = true;
@@ -79,8 +60,6 @@ public class Day6_part2 {
                 }
             }
         }
-
-        
         System.out.println("Answer = " + answer);
     }
 }
