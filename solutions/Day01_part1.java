5a458ddf468f29b8931cfec49a398a18f578f3d6
Pawel Iskra <52103903+Pawel-Iskra@users.noreply.github.com>
2020-06-11 15:10:37 +0200

    Update Day01_part1.java

--- solutions/Day01_part1.java
+++ solutions/Day01_part1.java
@@ -1,19 +1,14 @@
 import java.io.*;
 
 public class Day1_part1 {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int sum = 0;
-
         String line;
         while ((line = br.readLine()) != null)
             sum = sum + Integer.parseInt(line) / 3 - 2;
 
-
         System.out.println("sum = " + sum);
-
     }
 }
