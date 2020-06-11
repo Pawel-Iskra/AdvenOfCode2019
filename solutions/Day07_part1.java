f9890546928e606f99fa7a277513c6c900fd65dc
Pawel Iskra <52103903+Pawel-Iskra@users.noreply.github.com>
2020-06-11 15:48:38 +0200

    Update Day07_part1.java

--- solutions/Day07_part1.java
+++ solutions/Day07_part1.java
@@ -7,57 +7,40 @@ import java.util.List;
 import java.util.Random;
 
 public class Day7_part1 {
-
     private static int intComputer(int[] in, int phase, int input) {
-
         int out = 0;
         int inputIter = 0;
-
         int i = 0;
         while (in[i] != 99) {
-
             int temp = in[i];
             int[] mode = new int[3];
-
             mode[0] = temp / 10000;
             mode[1] = (temp / 1000) % 10;
             mode[2] = (temp / 100) % 10;
-
             int opCode = temp % 10;
-
             switch (opCode) {
-
                 case 1:
                 case 2:
                     int numb1 = 0, numb2 = 0;
-
                     i++;
                     if (mode[2] == 0) numb1 = in[in[i]];
                     if (mode[2] == 1) numb1 = in[i];
-
                     i++;
                     if (mode[1] == 0) numb2 = in[in[i]];
                     if (mode[1] == 1) numb2 = in[i];
-
                     i++;
                     if (opCode == 1) in[in[i]] = numb1 + numb2;
                     if (opCode == 2) in[in[i]] = numb1 * numb2;
-
                     i++;
                     break;
-
                 case 3:
-
                     i++;
                     if (inputIter == 0) in[in[i]] = phase;
                     if (inputIter == 1) in[in[i]] = input;
-
                     inputIter++;
                     i++;
                     break;
-
                 case 4:
-
                     i++;
                     int param = -100;
                     if (mode[2] == 0) param = in[i];
@@ -65,19 +48,15 @@ public class Day7_part1 {
                     out = in[param];
                     i++;
                     break;
-
                 case 5:
                 case 6:
-
                     i++;
                     int firstParam = 0, secParam = 0;
                     if (mode[2] == 0) firstParam = in[in[i]];
                     if (mode[2] == 1) firstParam = in[i];
-
                     i++;
                     if (mode[1] == 0) secParam = in[in[i]];
                     if (mode[1] == 1) secParam = in[i];
-
                     if (opCode == 5) {
                         if (firstParam != 0) i = secParam;
                         else i++;
@@ -87,20 +66,15 @@ public class Day7_part1 {
                         else i++;
                     }
                     break;
-
                 case 7:
                 case 8:
-
                     int param1 = 0, param2 = 0;
-
                     i++;
                     if (mode[2] == 0) param1 = in[in[i]];
                     if (mode[2] == 1) param1 = in[i];
-
                     i++;
                     if (mode[1] == 0) param2 = in[in[i]];
                     if (mode[1] == 1) param2 = in[i];
-
                     i++;
                     if (opCode == 7) {
                         if (param1 < param2) in[in[i]] = 1;
@@ -118,48 +92,34 @@ public class Day7_part1 {
     }
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] input = br.readLine().split(",");
         int x = input.length;
         int[] in = new int[x];
-
         for (int i = 0; i < x; i++)
             in[i] = Integer.parseInt(input[i]);
-
-
         Random rand = new Random();
-
         List<Integer> outList = new ArrayList<>();
         int out;
-
         for (int i = 0; i < 10000; i++) {
-
             int ph1 = rand.nextInt(5);
             out = intComputer(in, ph1, 0);
-
             int ph2 = rand.nextInt(5);
             while (ph2 == ph1) ph2 = rand.nextInt(5);
             out = intComputer(in, ph2, out);
-
             int ph3 = rand.nextInt(5);
             while (ph3 == ph2 || ph3 == ph1) ph3 = rand.nextInt(5);
             out = intComputer(in, ph3, out);
-
             int ph4 = rand.nextInt(5);
             while (ph4 == ph3 || ph4 == ph2 || ph4 == ph1) ph4 = rand.nextInt(5);
             out = intComputer(in, ph4, out);
-
             int ph5 = rand.nextInt(5);
             while (ph5 == ph4 || ph5 == ph3 || ph5 == ph2 || ph5 == ph1) ph5 = rand.nextInt(5);
             out = intComputer(in, ph5, out);
-
             outList.add(out);
         }
-
         Collections.sort(outList);
         System.out.println("Answer = " + outList.get(outList.size() - 1));
     }
 }
-
