dd73eaefaa2bbf6facbff4bd892da4e789f3113c
Pawel Iskra <52103903+Pawel-Iskra@users.noreply.github.com>
2020-06-11 15:22:35 +0200

    Update Day02_part1.java

--- solutions/Day02_part1.java
+++ solutions/Day02_part1.java
@@ -9,26 +9,18 @@ import java.util.Random;
 
 public class Day2_part1 {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] input = br.readLine().split(",");
-
         List<Integer> in = new ArrayList<>();
-
         int size = input.length;
         for (int i = 0; i < size; i++)
             in.add(Integer.parseInt(input[i]));
-
-
         in.set(1, 12);
         in.set(2, 2);
-
         int i = 0;
         while (in.get(i) != 99) {
-
             switch (in.get(i)) {
-
                 case 1:
                     in.set(in.get(i + 3), in.get(in.get(i + 1)) + in.get(in.get(i + 2)));
                     break;
@@ -38,8 +30,6 @@ public class Day2_part1 {
             }
             i = i + 4;
         }
-
         System.out.println("Answer = " + in.get(0));
-
     }
 }
