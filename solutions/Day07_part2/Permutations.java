6d739a71afd48c67b10ed703d30c5b9a3cd0eb9f
Pawel Iskra <52103903+Pawel-Iskra@users.noreply.github.com>
2020-06-11 15:52:20 +0200

    Update Permutations.java

--- solutions/Day07_part2/Permutations.java
+++ solutions/Day07_part2/Permutations.java
@@ -4,12 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Permutations {
-
     private List<int[]> outList = new ArrayList<>();
     public List<int[]> getOutList() {
         return outList;
     }
-
     void permute(int[] tab, int start) {
         if (tab.length == start) {
             outList.add(tab);
@@ -23,6 +21,4 @@ public class Permutations {
             }
         }
     }
-
-
 }
