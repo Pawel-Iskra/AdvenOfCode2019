aa8228b555cbeac651da3079d700e0591ab769b5
Pawel Iskra <52103903+Pawel-Iskra@users.noreply.github.com>
2020-06-11 15:32:02 +0200

    Update Day04_part1.java

--- solutions/Day04_part1.java
+++ solutions/Day04_part1.java
@@ -3,25 +3,18 @@ import java.util.Set;
 
 public class Day4_part1 {
     public static void main(String[] args) {
-
         int start = 235741;
         int end = 706948;
 
         Set<Integer> setRand = new HashSet<>();
-
         for (int i = start; i <= end; i++) {
-
             boolean flag1 = false, flag2 = false;
-
             char[] lc = String.valueOf(i).toCharArray();
-
             if (lc[0] == lc[1] || lc[1] == lc[2] || lc[2] == lc[3] || lc[3] == lc[4] || lc[4] == lc[5]) flag1 = true;
             if (lc[0] <= lc[1] && lc[1] <= lc[2] && lc[2] <= lc[3] && lc[3] <= lc[4] && lc[4] <= lc[5]) flag2 = true;
 
             if (flag1 && flag2) setRand.add(i);
         }
-
         System.out.println("Answer = " + setRand.size());
-
     }
 }
